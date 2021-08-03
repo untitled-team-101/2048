dd47a3ba925f30a03d4fc1d46170349cc51a684c
Ayan Dhara <37933140+Ayan-Dhara@users.noreply.github.com>
2021-08-04 02:12:24 +0530

    Fixed game reset and game over
    1. Fixed separate high scores for separate levels
    2. Fixed reset of game (added choose level)
    3. Fixed game over issue (of showing choose level)
    4. Changed choose level UI
    5. Removed build

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,24 +1,24 @@
 import endCred from '../video/video_3.webm'
 import '../style/GameOver.scss'
 import {Redirect} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const GameOver = () => {
-    const [cred, setCred] = useState(false);
-    localStorage.removeItem("grid");
-    localStorage.setItem("score", "0");
+  const [cred, setCred] = useState(false);
+  useEffect(() => {
     setTimeout(() => {
-        setCred(true)
-        console.log(cred);
+      setCred(true)
     }, 8000);
-    return (
-        <div className={'endCredits'}>
-            <video src={endCred} autoPlay={true}>wow</video>
-            {
-                cred ? <Redirect to={"/credits"}/> : null
-            }
-        </div>
-    )
+  })
+
+  return (
+    <div className={'endCredits'}>
+      <video src={endCred} autoPlay={true}>wow</video>
+      {
+        cred ? <Redirect to={"/credits"}/> : null
+      }
+    </div>
+  )
 }
 
 export default GameOver;
